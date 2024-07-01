c7610a7430bf7c5ba323826edbf66312c31462c2
huaming <huaming.me@gmail.com>
2024-07-01 15:02:13 +0800

    OK

--- composeApp/src/desktopMain/kotlin/com/chm/stockkankan/StockInfoWindow.kt
+++ composeApp/src/desktopMain/kotlin/com/chm/stockkankan/StockInfoWindow.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpSize
